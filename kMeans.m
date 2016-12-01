d0667360daf777cb87ef1e7a834b936fac868e04
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2016-12-01 17:18:45 +0800

    optimize kmeans to have a much better performance

--- kMeans.m
+++ kMeans.m
@@ -6,57 +6,59 @@ function [ result, finalCentric, iterationTime ] = kMeans( dataSet,k,comd,vararg
     order = 0;
     switch comd
         case 'E'
-            fun = @(x,y,p)pdist([x;y]);
+            fun = @(c,d,p)pdist2(c,d,'squaredeuclidean','Smallest',1);
         case 'V'
-            fun = @(x,y,p)pdist([x;y],'cosine');
+            fun = @(c,d,p)pdist2(c,d,'cosin','Smallest',1);
         case 'M'
-            fun = @(x,y,p)pdist([x;y],'minkowski',p);
             order = varargin{1};
+            fun = @(c,d,p)pdist2(c,d,'minkowski',p,'Smallest',1)
         case 'C'
-            fun = @(x,y,p)pdist([x;y],'correlation');
+            fun = @(x,y,p)pdist2(c,d,'correlation','Smallest',1);
     end
     centricSet = zeros(k,dimension - 1);
     newCentricSet = zeros(k,dimension - 1);
     
-    uniqueLength = 0;
-    classCell = cell(k,1);   %store vector in class
-    indexVec = ones(k,1);    %store current num in each class
-    iterationTime = 0;
+%     uniqueLength = 0;
+%    classCell = cell(k,1);   %store vector in class
+%     indexVec = ones(k,1);    %store current num in each class
+    iterationTime = 1;
     
-    while k ~= uniqueLength    %make sure no same value generated
-        randV = round(rand(k,1)*count);
-        [uniqueLength,~] = size(unique(randV));
-    end
-    
-    centricSet = dataSet(randV,2:dimension);
-    
-    for m = 1:1:k
-        classCell{m} = zeros(count,dimension);
-    end
+%     while k ~= uniqueLength    %make sure no same value generated
+%         randV = round(rand(k,1)*count);
+%         [uniqueLength,~] = size(unique(randV));
+%     end
+%     
+%     centricSet = dataSet(randV,2:dimension);
+    centricSet = dataSet(randsample(1:count,k),2:dimension);
+    indexTag = zeros(count,1);
+%     for m = 1:1:k
+%         classCell{m} = zeros(count,dimension);
+%     end
     
     while (iterationTime < 3000)
-        if iterationTime ~= 0
-            centricSet = newCentricSet;
-        end
-        for m = 1:1:count
-            index = getMinDisIndex(dataSet(m,2:dimension),centricSet,fun,order);
-            classCell{index}(indexVec(index),:) = dataSet(m,:);
-            indexVec(index) = indexVec(index) + 1;
-        end
+        [~,indexTag] = fun(centricSet,dataSet(:,2:dimension),order);
+%         if iterationTime ~= 0
+%             centricSet = newCentricSet;
+%         end
+%         for m = 1:1:count
+%             index = getMinDisIndex(dataSet(m,2:dimension),centricSet,fun,order);
+%             classCell{index}(indexVec(index),:) = dataSet(m,:);
+%             indexVec(index) = indexVec(index) + 1;
+%         end
         for m = 1:1:k
-            newCentricSet(m,:) = mean(classCell{m}(1:indexVec(m)-1,2:dimension));
+            newCentricSet(m,:) = mean(dataSet(indexTag==m,2:dimension));
         end
-        
+%         
         if isequal(newCentricSet,centricSet)
             break;
         else
             iterationTime = iterationTime + 1;
-            indexVec = ones(k,1);
-            for m = 1:1:k
-                classCell{m} = zeros(count,dimension);
-            end    
-        end
-        
+            centricSet = newCentricSet;
+%             indexVec = ones(k,1);
+%             for m = 1:1:k
+%                 classCell{m} = zeros(count,dimension);
+%             end    
+        end      
     end
     
     disp(strcat('Iteration Time: ',num2str(iterationTime)));
@@ -72,20 +74,21 @@ function [ result, finalCentric, iterationTime ] = kMeans( dataSet,k,comd,vararg
     
     result = cell(k,1);
     for m=1:1:k
-        classCell{m}(indexVec(m):count,:)=[];
-        result{m} = classCell{m};
+%         classCell{m}(indexVec(m):count,:)=[];
+        result{m} = dataSet(indexTag==m,2:dimension);
     end
     
     finalCentric = newCentricSet; 
 end
 
 
-function [index] = getMinDisIndex(vec,centricSet,fun,p)
-    [k,~] = size(centricSet);
-    dis = zeros(k,1);
-    for m=1:1:k
-        dis(m) = fun(vec,centricSet(m,:),p);
-    end
-    [~,index]=min(dis);
-end
+% function [index] = getMinDisIndex(vec,centricSet,fun,p)
+%     [k,~] = size(centricSet);
+%     dis = zeros(k,1);
+%     for m=1:1:k
+%         dis(m) = fun(vec,centricSet(m,:),p);
+%     end
+%     [~,index]=min(dis);
+% end
+
 
