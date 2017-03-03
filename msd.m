ddcbb43d423382b2805bf7f713cf0729f0a49601
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2017-03-03 17:04:56 +0800

    add moment order set

--- msd.m
+++ msd.m
@@ -1,4 +1,10 @@
-function [ result ] = msd( vec,lag )
+function [ result ] = msd( vec,lag,varargin )
+    if nargin == 2
+        p = 2;
+    else if nargin == 3
+            p = varargin{1};
+        end
+    end
     [nr,~] = size(vec);
     result = zeros(lag,1);
     gIndexM = @(calLength,tau)bsxfun(@plus,(1:1:calLength)',[0,tau]);
@@ -7,7 +13,7 @@ function [ result ] = msd( vec,lag )
         vecIndex = gIndexM(calLength,m);
         tmpM_L = vec(vecIndex(:,1),:);
         tmpM_H = vec(vecIndex(:,2),:);
-        result(m) = mean(sum((tmpM_H - tmpM_L).^2,2));
+        result(m) = mean(sum((abs(tmpM_H - tmpM_L)).^p,2));
     end
 end
 
