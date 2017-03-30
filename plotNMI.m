f4a40eaba42cb4e646549015468691251ddf5bd3
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2017-03-30 22:26:20 +0800

    init

---
+++ plotNMI.m
@@ -0,0 +1,19 @@
+function [ res ] = plotNMI(vec,maxT,nbins)
+    precious = range(vec)/nbins;
+    res = zeros(maxT+1,1);
+    vec = round((vec./precious)) .* precious; 
+    for m = 0:1:maxT
+        newLength = length(vec) - m;
+        res(m+1) = nmi(vec(1:newLength)',vec((1+m):end)');
+        %fprintf(1,'%d has done\n',m);
+    end
+    plot(0:1:maxT,res,'LineWidth',1);
+    xlabel('Time Delay');
+    ylabel('Normalize Mutual Information');
+    [~,loc] = findpeaks(-res);
+    if ~isempty(loc)
+        hold on;
+        scatter(loc(1)-1,res(loc(1)),'filled');
+    end
+end
+
