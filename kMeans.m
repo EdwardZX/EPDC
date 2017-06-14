7b3eeb5f72e0cf86d641632b395dde983c34827c
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2017-06-14 15:44:27 +0800

    add group number limit

--- kMeans.m
+++ kMeans.m
@@ -2,6 +2,13 @@
 % 2016/12/2 : version 2.1
 function [ indexTag, finalCentric, Distance ] = kMeans( dataSet,k,comd,varargin )
 
+    if nargin <= 4
+        isCheckMemberNum = false;
+    else
+        isCheckMemberNum = true;
+        threshold = varargin{2};
+    end
+    
     [count,dimension] = size(dataSet);
     order = 0;
     switch comd
@@ -38,13 +45,48 @@ function [ indexTag, finalCentric, Distance ] = kMeans( dataSet,k,comd,varargin 
             centricSet = sortMatrix(newCentricSet);   
         end      
     end
+    if isCheckMemberNum
+        if threshold < 1
+            threshold = round(threshold*count/k);
+            fprintf(1,'Limit number: %d\n',threshold);
+        end
+        gcounts = zeros(k,1);
+        for m = 1:1:k
+            gcounts(m) = sum(indexTag==m);
+        end
+        [x,I] = min(gcounts);
+        if x < threshold
+            fprintf(2,'Group: %d has %d member, too short for %d average group count\n',...
+                    I,x,threshold);
+            filter = (indexTag==I);
+            indexTag(filter) = nan;
+            [newTag,newC,newD] = kMeans(dataSet(~filter,:),k,comd,varargin{1},threshold);
+            indexTag(~filter) = newTag;
+            finalCentric = newC;
+            Distance = newD;
+            return;
+        end
+
+%         for m = 1:1:k
+%             filter = (indexTag==m);
+%             memberCount = sum(filter);
+%             if memberCount < threshold
+%                 fprintf(2,'Group: %d has %d member, too short for %d average group count\n',...
+%                     m,memberCount,threshold);
+%                 tmpTag = indexTag;
+%                 tmpTag(filter) = nan;
+%                 [newTag,newC,newD] = kMeans(dataSet(~filter,:),k,comd,varargin{1},threshold);
+%                 tmpTag(~filter) = newTag;
+%                 indexTag = tmpTag;
+%                 finalCentric = newC;
+%                 Distance = newD;
+%                 return;
+%             end
+%         end
+    end
     
     Distance = sum(D);
     finalCentric = newCentricSet;
-    
-    %disp(strcat('Iteration Time: ',num2str(iterationTime)));
-    %disp(strcat('sum of distance: ',num2str(Distance)));
-
 end
 
 function [M] = sortMatrix(M_I)
