1d741b951a13d0346abd5e997d33586c71b4c7b1
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2017-06-14 15:46:42 +0800

    add optimize time

--- pNPa.m
+++ pNPa.m
@@ -1,7 +1,7 @@
 % Hansen Zhao : zhaohs12@163.com
 % 2016/12/3 : version 2.0
 % IN Tsinghua
-% function [ result ] = pNPa(xy,rawData,analysisMethod,param,comd,varargin)
+% function [ result ] = pNPa(xy,rawData,analysisMethod,param,comd,optTime,isMute,varargin)
 % xy : N-by-2 particle position in time series
 % rawData : N-by-M raw data in time series
 % analysisMethod:
@@ -18,8 +18,8 @@
 % 'V' for dot product
 % 'M' for Minkowski distance
 % 'C' for correlation
-% varargin{1} optimize time in kMeans
-function [ result ] = pNPa(xy,rawData,analysisMethod,param,comd,varargin)
+% varargin{1} group number ratio limit in kmeans
+function [ result ] = pNPa(xy,rawData,analysisMethod,param,comd,optTime,isSpeak,varargin)
     [count,~] = size(param);
     result = cell(count,1);
     if any(any(isnan(xy))) || any(any(isnan(rawData))) || any(any(isnan(param)))
@@ -38,46 +38,30 @@ function [ result ] = pNPa(xy,rawData,analysisMethod,param,comd,varargin)
         case 'multi'
             aFun = @(raw,tau,D)multiVar2CM(raw,tau,D);
     end
-    isOpt = ~isempty(varargin);
 
     velocity = zeros(size(xy,1),1);
     velocity(2:end) = bsxfun(@(x,y)(sum((x-y).*(x-y),2)).^0.5,xy(2:end,:),xy(1:(end-1),:));
     
     for m=1:1:count
         [data,header] = aFun(rawData,param(m,1),param(m,2));
-        if isOpt
-          [I,C,~] = optKMeans(data,param(m,3),comd,param(m,4),varargin{1});
+        if isempty(varargin)
+          [I,C,~] = optKMeans(data,param(m,3),comd,param(m,4),optTime);
         else
-          [I,C,~] = optKMeans(data,param(m,3),comd,param(m,4),1);
+          [I,C,~] = optKMeans(data,param(m,3),comd,param(m,4),optTime,varargin{1});
         end
         result{m} = NPMotionTest(analysisMethod,xy,data,I,header,C,velocity,param(m,1),param(m,2),param(m,3));
         disp(strcat(num2str(m),' / ',num2str(count),' has been done!'));
-        if strcmp(analysisMethod,'uni')
-            result{m}.plot(rawData);
-        else
-            result{m}.plot();
+        if isSpeak
+            if strcmp(analysisMethod,'uni')
+                result{m}.plot(rawData);
+            else
+                result{m}.plot();
+            end
         end
     end
     if count == 1
         result = result{1};
     end
     disp('All process has been done!');
-end
-
-function [indexTag,C,D] = optKMeans(raw,k,comd,p,optTime)
-    disp('Optimization begin...');
-    disp(strcat('Total trial: ',num2str(optTime)));
-    [indexTag,C,D] = kMeans(raw,k,comd,p);
-    if optTime > 1
-        for m = 2:1:optTime
-            [I,c,d] = kMeans(raw,k,comd,p);
-            if(d<D)
-                indexTag = I;
-                C = c;
-                D = d;
-            end
-        end
-        disp(strcat('The optimized distance is: ',num2str(D)));
-    end    
 end
 
