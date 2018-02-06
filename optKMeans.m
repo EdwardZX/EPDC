5213399d1483c3c8d150ba3bf789d1362f0bb1ab
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2018-02-06 19:37:05 +0800

    add prob

--- optKMeans.m
+++ optKMeans.m
@@ -1,23 +1,24 @@
-function [indexTag,C,D] = optKMeans(raw,k,comd,p,optTime,varargin)
+function [indexTag,C,D,Prob] = optKMeans(raw,k,comd,p,optTime,varargin)
     %% function [indexTag,C,D] = optKMeans(raw,k,comd,p,optTime,varargin)
     disp('Optimization begin...');
     disp(strcat('Total trial: ',num2str(optTime)));
     if isempty(varargin)
-        [indexTag,C,D] = kMeans(raw,k,comd,p);
+        [indexTag,C,D,Prob] = kMeans(raw,k,comd,p);
     else
-        [indexTag,C,D] = kMeans(raw,k,comd,p,varargin{1});
+        [indexTag,C,D,Prob] = kMeans(raw,k,comd,p,varargin{1});
     end
     if optTime > 1
         for m = 2:1:optTime
             if isempty(varargin)
-                [I,c,d] = kMeans(raw,k,comd,p);
+                [I,c,d,prob] = kMeans(raw,k,comd,p);
             else
-                [I,c,d] = kMeans(raw,k,comd,p,varargin{1});
+                [I,c,d,prob] = kMeans(raw,k,comd,p,varargin{1});
             end
             if(d<D)
                 indexTag = I;
                 C = c;
                 D = d;
+                Prob = prob;
             end
         end
         disp(strcat('The optimized distance is: ',num2str(D)));
