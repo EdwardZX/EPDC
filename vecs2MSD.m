752f77870bdc782b702c0dae79ebd7a16070c4a8
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2018-02-06 19:40:21 +0800

    new algorithm

--- vecs2MSD.m
+++ vecs2MSD.m
@@ -1,6 +1,6 @@
 function [msdMatrix,header] = vecs2MSD(vecs,D)
-    msdFun = @(A,B)mean(sum((A-B).^2,2));
-    gIndexM = @(Index,D)bsxfun(@minus,(Index:-1:Index-D+1)',0:D);
+    %msdFun = @(A,B)mean(sum((A-B).^2,2));
+    %gIndexM = @(Index,D)bsxfun(@minus,(Index:-1:Index-D+1)',0:D);
     rawLength = size(vecs,1);
     dTime = 2 * D - 1;
     header = 2 * D;
@@ -8,9 +8,10 @@ function [msdMatrix,header] = vecs2MSD(vecs,D)
     msdMatrix = zeros(newLength,D);
     
     for m = 1:newLength
-        IM = gIndexM(m + dTime,D);
-        for n = 1:D
-            msdMatrix(m,n) = msdFun(vecs(IM(:,1),:),vecs(IM(:,1+n),:));
-        end
+        msdMatrix(m,:) = msd(vecs(m:1:(m+dTime),:),D)';
+%         IM = gIndexM(m + dTime,D);
+%         for n = 1:D
+%             msdMatrix(m,n) = msdFun(vecs(IM(:,1),:),vecs(IM(:,1+n),:));
+%         end
     end
 end
